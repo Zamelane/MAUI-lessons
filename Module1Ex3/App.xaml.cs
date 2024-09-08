1431c96c740d87a970ae4cb146074691b2e4997f
zamelane <zamelane@vk.com>
2024-09-08 18:15:56 +0700

    first commit

---
+++ Module1Ex3/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace Module1Ex3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+        }
+    }
+}
---
+++ Module1Ex3/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Module1Ex3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
